c23cf8d7579237d934b2282443b2a97aa8ff85b3
root <root@erp.binhex.es>
2021-01-18 11:20:00 +0000

    arreglando errores instalar

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'report/views.xml',
         # 'views/templates.xml',
         'views/projectreports.xml', 
         # 'views/projectreports_project.xml'
